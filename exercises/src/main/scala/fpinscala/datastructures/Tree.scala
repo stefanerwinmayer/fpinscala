2752341a3cd98764f01eb35fb8b2974e90d10eb7
Stefan E. Mayer <stefan.e.mayer@gmail.com>
2019-05-21 19:35:28 +0100

    Apply scalafmt

--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -4,10 +4,4 @@ sealed trait Tree[+A]
 case class Leaf[A](value: A) extends Tree[A]
 case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
-
-object Tree {
-
-
-
-
-}
+object Tree {}
