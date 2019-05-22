c8b72b8f1ec07a86ec110f90af7ec0db24f84071
Stefan E. Mayer <stefan.e.mayer@gmail.com>
2019-05-22 19:25:33 +0100

    Implement init

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -76,7 +76,11 @@ object List { // `List` companion object. Contains functions for creating and wo
       case _                   => l
     }
 
-  def init[A](l: List[A]): List[A] = ???
+  def init[A](l: List[A]): List[A] =
+    l match {
+      case Nil | Cons(_, Nil) => Nil
+      case Cons(x, xs)        => Cons(x, init(xs))
+    }
 
   def length[A](l: List[A]): Int = ???
 
