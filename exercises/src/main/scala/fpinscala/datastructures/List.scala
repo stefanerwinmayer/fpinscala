2f25196a797b9a2fddb03ab99767a132988d9b99
Stefan E. Mayer <stefan.e.mayer@gmail.com>
2019-05-22 21:16:29 +0100

    Implement length

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -82,7 +82,10 @@ object List { // `List` companion object. Contains functions for creating and wo
       case Cons(x, xs)        => Cons(x, init(xs))
     }
 
-  def length[A](l: List[A]): Int = ???
+  def length[A](l: List[A]): Int =
+    foldRight(l, 0) { (_, b) =>
+      b + 1
+    }
 
   def foldLeft[A, B](l: List[A], z: B)(f: (B, A) => B): B = ???
 
