a6f9f5ce25fc11cbf0e6502cffc4d8f56d470920
Stefan E. Mayer <stefan.e.mayer@gmail.com>
2019-06-02 16:22:43 +0100

    Implement filter in terms of flatMap

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -150,4 +150,9 @@ object List { // `List` companion object. Contains functions for creating and wo
 
   def flatMap[A, B](l: List[A])(f: A => List[B]): List[B] =
     flatConcat(map(l)(f))
+
+  def filter2[A](l: List[A])(p: A => Boolean): List[A] =
+    flatMap(l) { a =>
+      if (p(a)) List(a) else List()
+    }
 }
