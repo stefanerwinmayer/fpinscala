a4359453db582a1ca03a06a393249c94ff0f2630
Stefan E. Mayer <stefan.mayer@ovoenergy.com>
2019-06-05 11:33:26 +0100

    Implement traverse

--- exercises/src/main/scala/fpinscala/errorhandling/Option.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Option.scala
@@ -72,5 +72,11 @@ object Option {
       map2(a, b)(_ :: _)
     }
 
-  def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] = ???
+  def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] =
+    a.foldRight[Option[List[B]]](Some(Nil)) { (a, b) =>
+      map2(f(a), b)(_ :: _)
+    }
+
+  def sequenceFromTraverse[A](a: List[Option[A]]): Option[List[A]] =
+    traverse(a)(x => x)
 }
