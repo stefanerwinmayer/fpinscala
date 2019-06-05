9f0c1ec082ca50d4180f9217cf4a2b8a764512f9
Stefan E. Mayer <stefan.mayer@ovoenergy.com>
2019-06-05 12:29:32 +0100

    Implement several Either methods

--- exercises/src/main/scala/fpinscala/errorhandling/Either.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Either.scala
@@ -9,13 +9,29 @@ import scala.{
 } // hide std library `Option` and `Either`, since we are writing our own in this chapter
 
 sealed trait Either[+E, +A] {
-  def map[B](f: A => B): Either[E, B] = ???
+  def map[B](f: A => B): Either[E, B] =
+    this match {
+      case Left(e)  => Left(e)
+      case Right(a) => Right(f(a))
+    }
 
-  def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = ???
+  def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] =
+    this match {
+      case Left(e)  => Left(e)
+      case Right(a) => f(a)
+    }
 
-  def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = ???
+  def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] =
+    this match {
+      case Left(_)  => b
+      case Right(a) => Right(a)
+    }
 
-  def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = ???
+  def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] =
+    for {
+      aa <- this
+      bb <- b
+    } yield f(aa, bb)
 }
 case class Left[+E](get: E) extends Either[E, Nothing]
 case class Right[+A](get: A) extends Either[Nothing, A]
