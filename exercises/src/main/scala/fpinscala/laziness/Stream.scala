94d3470e2d7c85e8fd57e4444645cbf1c6bc7f1c
Stefan E. Mayer <stefan.mayer@ovoenergy.com>
2019-06-06 13:10:56 +0100

    Implement forAll

--- exercises/src/main/scala/fpinscala/laziness/Stream.scala
+++ exercises/src/main/scala/fpinscala/laziness/Stream.scala
@@ -46,7 +46,10 @@ trait Stream[+A] {
       case _                     => empty
     }
 
-  def forAll(p: A => Boolean): Boolean = ???
+  def forAll(p: A => Boolean): Boolean =
+    foldRight(true) { (a, b) =>
+      p(a) && b
+    }
 
   def headOption: Option[A] = ???
 
