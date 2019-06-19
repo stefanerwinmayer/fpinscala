5dd389c818cd8c2e00e28080babaceea072d3d55
Stefan E. Mayer <stefan.mayer@ovoenergy.com>
2019-06-19 16:05:08 +0100

    Implement scanRight

--- exercises/src/main/scala/fpinscala/laziness/Stream.scala
+++ exercises/src/main/scala/fpinscala/laziness/Stream.scala
@@ -129,6 +129,15 @@ trait Stream[+A] {
       case Empty => None
       case s     => Some((s, s drop 1))
     }
+
+  def scanRight[B](z: B)(f: (A, => B) => B): Stream[B] =
+    foldRight((z, Stream(z))) { (a, p0) =>
+      {
+        lazy val p1 = p0
+        val b2 = f(a, p1._1)
+        (b2, cons(b2, p1._2))
+      }
+    }._2
 }
 case object Empty extends Stream[Nothing]
 case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
